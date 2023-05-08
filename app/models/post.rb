ccf41df861885dc0a05206a7b099551e695b360c
Anton Samoshin <asamoshin9@gmail.com>
2023-05-08 16:29:18 +0300

    Remove dependent option from models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@
 class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_many :comments, class_name: 'PostComment', dependent: :destroy
-  has_many :likes, class_name: 'PostLike', dependent: :destroy
+  has_many :comments, class_name: 'PostComment'
+  has_many :likes, class_name: 'PostLike'
 end
