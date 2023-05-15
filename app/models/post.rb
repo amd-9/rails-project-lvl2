a557b0a8238ecc09e1506909f1f5b07b000d9c72
Anton Samoshin <asamoshin9@gmail.com>
2023-05-15 16:34:52 +0300

    Update inverse associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { in: 200..4000 }
 
   belongs_to :category
-  belongs_to :creator, class_name: 'User', foreign_key: :user_id, inverse_of: false
+  belongs_to :creator, class_name: 'User', foreign_key: :user_id, inverse_of: :posts
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, dependent: :destroy, inverse_of: false
+  has_many :posts, dependent: :destroy, inverse_of: :creator
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
