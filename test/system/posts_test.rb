64d1f44e408e14345f345ea9f17eb64a78321e8d
Anton Samoshin <asamoshin9@gmail.com>
2023-05-16 21:38:51 +0300

    Add i18n to system tests

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -13,16 +13,16 @@ class PostsTest < ApplicationSystemTestCase
 
   test 'visiting the index' do
     visit posts_url
-    assert_selector 'h1', text: 'Posts'
+    assert_selector 'h1', text: I18n.t('.posts.index.title')
   end
 
   test 'should create post' do
     visit posts_url
-    click_on 'New post'
+    click_on I18n.t '.posts.new.title'
 
-    fill_in 'Body', with: @post.body
-    fill_in 'Title', with: @post.title
-    select 'HiTech', from: 'Category'
+    fill_in I18n.t('.simple_form.labels.post.body'), with: @post.body
+    fill_in I18n.t('.simple_form.labels.post.title'), with: @post.title
+    select 'HiTech', from: I18n.t('.simple_form.labels.post.category')
     click_on 'Create Post'
 
     assert_text I18n.t '.post.create.success'
