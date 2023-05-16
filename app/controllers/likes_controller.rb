6aa2d14cefaa69b11196390c9caa35d0cf1c9542
Anton Samoshin <asamoshin9@gmail.com>
2023-05-16 21:12:42 +0300

    Fix likes error flash messages

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,20 +6,20 @@ class LikesController < ApplicationController
   def create
     @post_id = params[:post_id]
     @fail_message = t('.fail')
-    return redirect_to post_url(@post_id), error: @fail_message if PostLike.exists?(post_id: @post_id,
+    return redirect_to post_url(@post_id), alert: @fail_message if PostLike.exists?(post_id: @post_id,
                                                                                     user: current_user)
 
     @like = PostLike.new(user: current_user, post_id: @post_id)
 
     return redirect_to post_url(@like.post_id) if @like.save
 
-    redirect_to post_url(@like.post_id), error: @fail_message
+    redirect_to post_url(@like.post_id), alert: @fail_message
   end
 
   def destroy
     @like = PostLike.find(params[:id])
 
-    return redirect_to post_url(@like.post_id), error: t('.fail') if @like.user != current_user
+    return redirect_to post_url(@like.post_id), alert: t('.fail') if @like.user != current_user
 
     @like.destroy
 
